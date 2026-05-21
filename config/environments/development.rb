1f79ecbacf01f7c4633c57f5dc044598ab30d4b0
Valeriy Abramov <valeraabramov763@gmail.com>
2026-05-21 09:12:25 +0000

    запуск сайта

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,10 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
+  # Allow local access via localhost.
+  config.hosts << "localhost"
+  config.hosts << "shiny-zebra-r4pppp6qwvqxc66x-3000.app.github.dev"
+
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
